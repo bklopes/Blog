2b106db6582ed3fd6ae90fa43d413060f0c397c6
bklopes <brendan.lopes@gmail.com>
2015-02-04 22:27:32 -0500

    added simple database capabilities

---
+++ db_upgrade.py
@@ -0,0 +1,11 @@
+#script that applies the changes made to the database and completes the upgrade
+
+from migrate.versioning import api
+from config import SQLALCHEMY_DATABASE_URI
+from config import SQLALCHEMY_MIGRATE_REPO
+
+api.upgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+
+print('Current database verion: ' + str(v))
+
